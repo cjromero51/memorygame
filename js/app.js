87bbf2d5ba773a481e2fc907ea324dfa2e7f556b
CJ Romero <cjromero51@yahoo.com>
2018-05-29 19:02:02 -0700

    Only 2 unmatched cards can be shown at one time

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-
-let array = Array.prototype.slice.call(document.getElementsByClassName('card'));
+let array = Array.from(document.getElementsByClassName('card'));
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -26,18 +12,6 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 let cards = document.getElementsByClassName('card');
 let numberOfMoves = 0;
@@ -62,6 +36,13 @@ document.addEventListener('click', function(e) {
       targetElement[0].classList.remove('open');
       targetElement[0].classList.remove('open');
     }
+    if (document.getElementsByClassName('open').length > 2 || document.getElementsByClassName('mismatch').length > 2) {
+      let holdingArray = Array.from(document.getElementsByClassName('open'));
+      for (each of holdingArray) {
+        each.classList.remove('show');
+        each.classList.remove('open');
+      }
+    };
     // updates to either 'move' or 'moves' depending on plurality
     document.getElementById('moves').innerHTML = numberOfMoves;
     if (numberOfMoves === 1){
@@ -89,6 +70,14 @@ document.addEventListener('click', function(e) {
     targetElement[0].classList.add('mismatch');
     targetElement[1].classList.add('mismatch');
   }
+  if (document.getElementsByClassName('open').length > 2 || document.getElementsByClassName('mismatch').length > 2) {
+    let holdingArray = Array.from(document.getElementsByClassName('open'));
+    for (each of holdingArray) {
+      each.classList.remove('show');
+      each.classList.remove('mismatch');
+      each.classList.remove('open');
+    }
+  };
   // updates to either 'move' or 'moves' depending on plurality again
   if (targetElement.length === 2  && targetElement[0].childNodes[1].isEqualNode(targetElement[1].childNodes[1]) == false && targetElement[0].classList.contains('mismatch')) {
     numberOfMoves += 1;
@@ -99,6 +88,7 @@ document.addEventListener('click', function(e) {
       document.getElementById('plurality').innerHTML = plurality;
     };
   }});
+
 // game completed alert function and event listener
 function youDidIt() {
   alert('You did it in ' + numberOfMoves + ' moves!');
@@ -108,3 +98,10 @@ document.addEventListener('click', function(e) {
   if (document.getElementsByClassName('match').length === 16) {
     setTimeout(youDidIt, 1000);
   }});
+// if (document.getElementsByClassName('open').length > 2 || document.getElementsByClassName('mismatch').length > 2) {
+//   let holdingArray = Array.from(document.getElementsByClassName('open'));
+//   for (each of holdingArray) {
+//     each.classList.remove('open');
+//     each.classList.remove('mismatch');
+//   }
+// };
