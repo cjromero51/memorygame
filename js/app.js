feacc382bbabcaf953a8ec9d15fdfe07c42d0eea
CJ Romero <cjromero51@yahoo.com>
2018-05-29 18:13:19 -0700

    Correct plurality for move counter with "Move" or "Moves"

--- js/app.js
+++ js/app.js
@@ -41,15 +41,17 @@ function shuffle(array) {
 
 let cards = document.getElementsByClassName('card');
 let numberOfMoves = 0;
-let list = []
+let plurality = 'Moves';
+let singularity = 'Move';
 
+// applies addEventListener to all cards and adds classes when clicked
 for (let x = 0; x < 16; x++) {
   let revealedCards = cards[x]
   revealedCards.addEventListener('click', function(event) {
-    revealedCards.classList.toggle('open');
-    revealedCards.classList.toggle('show');
+    revealedCards.classList.add('open');
+    revealedCards.classList.add('show');
   })};
-
+// if two cards are clicked that match
 document.addEventListener('click', function(e) {
   let targetElement = document.getElementsByClassName('open');
   if (targetElement.length === 2  && targetElement[0].childNodes[1].isEqualNode(targetElement[1].childNodes[1])){
@@ -60,8 +62,16 @@ document.addEventListener('click', function(e) {
       targetElement[0].classList.remove('open');
       targetElement[0].classList.remove('open');
     }
+    // updates to either 'move' or 'moves' depending on plurality
+    document.getElementById('moves').innerHTML = numberOfMoves;
+    if (numberOfMoves === 1){
+      document.getElementById('plurality').innerHTML = singularity;
+    } else {
+      document.getElementById('plurality').innerHTML = plurality;
+    };
   }});
 
+// if two cards are clicked that don't match
 document.addEventListener('click', function(e) {
   let targetElement = document.getElementsByClassName('open');
   function waitFunction() {
@@ -79,16 +89,22 @@ document.addEventListener('click', function(e) {
     targetElement[0].classList.add('mismatch');
     targetElement[1].classList.add('mismatch');
   }
+  // updates to either 'move' or 'moves' depending on plurality again
   if (targetElement.length === 2  && targetElement[0].childNodes[1].isEqualNode(targetElement[1].childNodes[1]) == false && targetElement[0].classList.contains('mismatch')) {
     numberOfMoves += 1;
-  }
-});
+    document.getElementById('moves').innerHTML = numberOfMoves;
+    if (numberOfMoves === 1){
+      document.getElementById('plurality').innerHTML = singularity;
+    } else {
+      document.getElementById('plurality').innerHTML = plurality;
+    };
+  }});
+// game completed alert function and event listener
+function youDidIt() {
+  alert('You did it in ' + numberOfMoves + ' moves!');
+};
 
-  function youDidIt() {
-    alert('You did it in ' + numberOfMoves + ' moves!');
-  }
-  document.addEventListener('click', function(e) {
-    if (document.getElementsByClassName('match').length === 16) {
-      setTimeout(youDidIt, 1000);
-    }
-  })
+document.addEventListener('click', function(e) {
+  if (document.getElementsByClassName('match').length === 16) {
+    setTimeout(youDidIt, 1000);
+  }});
